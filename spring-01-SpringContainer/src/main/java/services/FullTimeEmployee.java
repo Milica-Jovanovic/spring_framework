9416722200b39c611a3a6492aedcb0642347b0e0
Milica Jovanovic <milica1205@gmail.com>
2020-10-21 17:06:12 -0400

    Spring Container

---
+++ spring-01-SpringContainer/src/main/java/services/FullTimeEmployee.java
@@ -0,0 +1,12 @@
+package services;
+
+import interfaces.Employee;
+
+public class FullTimeEmployee implements Employee {
+
+    @Override
+    public void createAccount() {
+        System.out.println("Full Time Mentor Account is created");
+    }
+
+}
---
+++ spring-01-SpringContainer/src/main/java/services/PartTimeEmployee.java
@@ -0,0 +1,12 @@
+package services;
+
+import interfaces.Employee;
+
+public class PartTimeEmployee implements Employee {
+
+    @Override
+    public void createAccount() {
+        System.out.println("Part Time Mentor account is created");
+    }
+
+}
