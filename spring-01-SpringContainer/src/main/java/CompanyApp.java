e8e5205870b825d2ce517ca1dfd2f3a11e95d4ae
Milica Jovanovic <milica1205@gmail.com>
2020-10-23 12:42:47 -0400

    Spring Container

---
+++ spring-01-SpringContainer/src/main/java/CompanyApp.java
@@ -0,0 +1,27 @@
+import interfaces.Employee;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class CompanyApp {
+
+    public static void main(String[] args) {
+
+
+        BeanFactory container1 = new ClassPathXmlApplicationContext("config.xml");
+
+        Employee employee1 = (Employee) container1.getBean("fullTimeEmployee");
+
+        employee1.createAccount();
+
+
+
+        ApplicationContext container2 = new ClassPathXmlApplicationContext("config.xml");
+
+        Employee employee2 = container2.getBean("partTimeEmployee",Employee.class);
+
+        employee2.createAccount();
+
+
+    }
+}
