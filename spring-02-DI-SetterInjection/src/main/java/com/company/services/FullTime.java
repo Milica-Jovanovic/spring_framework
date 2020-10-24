6033b09f5306ba5e6486f55e537f9365fb7ca19c
Milica Jovanovic <milica1205@gmail.com>
2020-10-24 17:57:22 -0400

    DI - Setter Injection

--- spring-02-DI-SetterInjection/src/main/java/com/company/services/FullTime.java
+++ spring-02-DI-SetterInjection/src/main/java/com/company/services/FullTime.java
@@ -2,16 +2,24 @@ package com.company.services;
 
 import com.company.interfaces.ExtraHours;
 import com.company.interfaces.Hours;
+import lombok.Data;
 
+@Data
 public class FullTime implements Hours {
+
+
 
     private OverTime overTime;
 
-    public  OverTime getExtraHours() {
+// command + N and choose getter and setter
+/*    public OverTime getOverTime() {
         return overTime;
     }
 
-
+    public void setOverTime(OverTime overTime) {
+        this.overTime = overTime;
+    }
+*/
     public void getHours() {
         System.out.println("Weekly Teaching Hours : " + (20 + overTime.getExtraHours()));
     }
