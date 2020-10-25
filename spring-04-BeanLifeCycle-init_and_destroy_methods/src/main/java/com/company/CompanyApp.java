03e45bc417a6350fa4900defccf2a5221a4c3bc1
Milica Jovanovic <milica1205@gmail.com>
2020-10-24 23:07:51 -0400

    Bean LifeCycle

---
+++ spring-04-BeanLifeCycle-init_and_destroy_methods/src/main/java/com/company/CompanyApp.java
@@ -0,0 +1,20 @@
+package com.company;
+
+import com.company.interfaces.Hours;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class CompanyApp {
+
+    public static void main(String[] args) {
+
+        ApplicationContext container = new ClassPathXmlApplicationContext("config.xml");
+
+        Hours hours = container.getBean("fullTime", Hours.class);
+
+        hours.getHours();
+
+        ((ClassPathXmlApplicationContext)container).close(); // down casting
+
+    }
+}
