8bded94fc778f57f0deae7d58596219a0e28fe85
Milica Jovanovic <milica1205@gmail.com>
2020-10-24 22:30:36 -0400

    Bean Scope

---
+++ spring-03-BeanScope-Singleton-Prototype/src/main/java/com/company/services/FullTime.java
@@ -0,0 +1,15 @@
+package com.company.services;
+
+import com.company.interfaces.Hours;
+import lombok.Data;
+
+@Data
+public class FullTime implements Hours {
+
+
+
+    public void getHours() {
+        System.out.println("Weekly Teaching Hours : 20");
+    }
+
+}
