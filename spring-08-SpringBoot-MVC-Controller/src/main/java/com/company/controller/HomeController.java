5a447cc7a0b45153d90ae080edf1ad33c8d4074a
Milica Jovanovic <milica1205@gmail.com>
2020-11-14 00:30:22 -0500

    MVC

--- spring-08-SpringBoot-MVC-Controller/src/main/java/com/company/controller/HomeController.java
+++ spring-08-SpringBoot-MVC-Controller/src/main/java/com/company/controller/HomeController.java
@@ -1,10 +1,46 @@
 package com.company.controller;
 
+import org.springframework.web.bind.annotation.*;
+
+import java.util.stream.Stream;
+
 public class HomeController {
 
+    @RequestMapping("/")
     public String getRequestMapping() {
 
         return "home";
     }
+
+    @RequestMapping("/Milica")
+    public String getRequestMapping2() {
+
+        return "home";
+    }
+
+    @RequestMapping(method= RequestMethod.GET, value="/Milica")
+    public String getRequestMapping3() {
+
+        return "home";
+    }
+
+    @GetMapping ("/spring")
+    public String getMappingExample() {
+
+        return "home";
+    }
+
+    @PostMapping ("/spring")
+    public String postMappingExample() {
+
+        return "home";
+    }
+
+    @GetMapping ("/spring/{name}")
+    public String pathVariableExample(@PathVariable("name")String name) {
+
+        return "home";
+    }
+
 
 }
