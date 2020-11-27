1d9f41979101c11c0b75ca0e0b55e22729340e38
Milica Jovanovic <milica1205@gmail.com>
2020-11-27 18:10:47 -0500

    MVC Form

--- spring-08-SpringBoot-MVC-Form/src/main/java/com/company/controller/EmployeeController.java
+++ spring-08-SpringBoot-MVC-Form/src/main/java/com/company/controller/EmployeeController.java
@@ -1,16 +1,27 @@
 package com.company.controller;
 
 
+import com.company.model.Employee;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Controller
 @RequestMapping("/employee")
 public class EmployeeController {
 
     @GetMapping("register")
-    public String showForm() {
+    public String showForm(Model model) {
+
+        model.addAttribute("employee", new Employee());
+
+        List<String> titleList = Arrays.asList("Front End Developer", "Back End Developer", "Full Stack Developer");
+        model.addAttribute("titleList", titleList);
+
 
         return "employee/employee-register";
     }
